5d59312d07f35243daded62cb689a7a14621af51
Andrew Cheon <cheonandrew113@gmail.com>
2019-09-24 21:16:53 -0400

    updated and complete

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,25 +5,68 @@ class Book extends React.Component {
 
   state = {
     showComments: false,
-    comments: []
+    comments: [],
+    newComment: "",
+    allComments: false,
+    commentsForm: false
   }
   
+
+  handleAddComment = () => {
+    this.setState({
+      commentsForm: !this.state.commentsForm
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleCommentSubmit = (event) => {
+    // console.log("Comment: ", this.state.newComment)
+    event.preventDefault()
+    this.setState({
+      comments: [...this.state.comments, this.state.newComment]
+    })
+    this.setState({
+      newComment: ""
+    })
+  }
+
+  handleShowComments = () => {
+    console.log("comments: ", this.state.comments.join(". "))
+    this.setState({
+      allComments: !this.state.allComments
+    })
+  }
+
+
   addBookShelf = () => {
     this.props.clickHandler(this.props.book)
   }
 
   render(){
 
-
+    const commentForm = 
+      <div>  
+        <textarea name = "newComment" placeholder = "Please add your comment here..." value = {this.state.newComment} onChange = {this.handleChange}/>
+        <br></br>
+        <button onClick = {this.handleCommentSubmit}>Submit</button>
+      </div>
 
 
     return (
       <div>
         <h2>{this.props.book.title}</h2>
         <img alt="" src={this.props.book.img} onClick={this.addBookShelf}/>
-        <button onClick={() => {this.setState({showComments: !this.state.showComments})}}>Add Comment</button>
-        <button>All comments</button>
-        {this.state.showComments? <div><textarea placeholder="add comment here"> </textarea><button>submit comment</button></div> : null}
+        <br/>
+        <button onClick = {this.handleAddComment}>Add Comment</button>
+          {this.state.commentsForm ? commentForm : null} 
+      
+        <button onClick = {this.handleShowComments}>Show comments</button>
+          {this.state.allComments ? <ul>{this.state.comments.map(comment => <li key = {comment}>{comment}</li>)}</ul> : null}
       </div>
     );
   }
